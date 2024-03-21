93bed9db394bec4b20007c8828780eca8939fbe9
Bethany Simmons-Emms <bethyse14@gmail.com>
2024-03-21 15:50:05 -0400

    updated index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = {
     secret: process.env.AUTH0_SECRET,
     baseURL: process.env.SITE_URL || 'http://localhost:3000',
     clientID: process.env.AUTH0_CLIENT_ID,
-    issuerBaseURL: 'https://bdaley.auth0.com'
+    issuerBaseURL: 'https://dev-zxpzg2qx6i3k5biy.us.auth0.com'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
